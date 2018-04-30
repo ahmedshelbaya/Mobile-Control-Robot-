c14348c1fab06968a481f6dc84cd1c55b88dc367
Ahmed Shelbaya <ahmedshelbaya15@gmail.com>
2018-05-01 01:07:28 +0200

    Add files via upload

---
+++ mobile_control_robot.ino
@@ -0,0 +1,70 @@
+
+int IN1=13;
+int IN2=12;
+int IN3=11;
+int IN4=10;
+int value;
+void setup()
+{
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+  
+  if(Serial.available()>0)
+  {
+    value=Serial.read();
+    switch(value)
+    {
+      case'F':forward();
+      break;
+      case'B':backward();
+      break;
+      case'R':right();
+      break;
+      case'L':left();
+      break;
+      default:STOP();
+    }
+  }
+}
+void forward()
+{
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+}
+void backward()
+{
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+}
+void right()
+{
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+}
+void left()
+{
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+}
+void STOP()
+{
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,LOW);
+}
+
